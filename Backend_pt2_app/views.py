91eb7b2bd1a34e632076819bb40c9f5c50896b1a
BiscuitBobby <87699062+BiscuitBobby@users.noreply.github.com>
2023-04-11 02:13:53 +0530

    added get api function

--- Backend_pt2_app/views.py
+++ Backend_pt2_app/views.py
@@ -1,6 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework import serializers
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from Backend_pt2_app.models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
 
 
 def index(request):
     return HttpResponse("welcome to the Index.")
+
+@api_view(['GET'])
+def getData(request):
+    token = request.META.get('HTTP_TOKEN')
+    if token == 'bob':
+        items = User.objects.all()
+        serializer = UserSerializer(items, many=True)
+        return Response(serializer.data)
+    else:
+        return request.META.get("Invalid token")
--- backend_pt2_project/urls.py
+++ backend_pt2_project/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path
+
 from Backend_pt2_app import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', views.index, name='index'),
+    path("users/", views.getData),
 ]
