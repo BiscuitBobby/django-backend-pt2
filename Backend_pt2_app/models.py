b718b004e554045767b91aa96610a6b6e9500130
BiscuitBobby <87699062+BiscuitBobby@users.noreply.github.com>
2023-04-11 15:19:09 +0530

    savepoint

--- Backend_pt2_app/models.py
+++ Backend_pt2_app/models.py
@@ -2,28 +2,15 @@ from django.contrib.auth.hashers import make_password
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin, Group, Permission
 from django.db import models
 from . import salt
-from django.contrib.auth.hashers import (
-    PBKDF2PasswordHasher, SHA1PasswordHasher,
-)
-
-
-class PBKDF2WrappedSHA1PasswordHasher(PBKDF2PasswordHasher):
-    algorithm = 'pbkdf2_wrapped_sha1'
-
-    def encode_sha1_hash(self, sha1_hash, salt, iterations=None):
-        return super().encode(sha1_hash, salt, iterations)
-
-    def encode(self, password, salt, iterations=None):
-        _, _, sha1_hash = SHA1PasswordHasher().encode(password, salt).split('$', 2)
-        return self.encode_sha1_hash(sha1_hash, salt, iterations)
-
+from .encryption import PBKDF2WrappedSHA1PasswordHasher
 
 
 class ManageUser(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
-        password_hasher = PBKDF2WrappedSHA1PasswordHasher()
         user = self.model(email=self.normalize_email(email)**extra_fields)
-        user.set_password(password_hasher.encode(password, salt.salt))
+        '''password_hasher = PBKDF2WrappedSHA1PasswordHasher()
+        user.set_password(password_hasher.encode(password, salt.salt))'''
+        user.set_password(password)
         user.save(using=self._db)
         return user
     def create_su(self, email, password=None, **extra_fields):
@@ -36,8 +23,6 @@ class User(AbstractBaseUser, PermissionsMixin):
     username = models.CharField(max_length=30, unique=True)
     is_superuser = models.BooleanField(default=False)
     date_joined = models.DateTimeField(auto_now_add=True)
-
-    username_field = 'email'
 
     # related_name argument
     groups = models.ManyToManyField(Group, blank=True, related_name='user_groups')
--- Backend_pt2_app/views.py
+++ Backend_pt2_app/views.py
@@ -1,11 +1,13 @@
 import traceback
-
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
 from rest_framework import serializers
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from Backend_pt2_app.models import User
+from . import salt
+from .encryption import PBKDF2WrappedSHA1PasswordHasher
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -17,25 +19,36 @@ class UserSerializer(serializers.ModelSerializer):
 def index(request):
     return HttpResponse("welcome to the Index.")
 
+'''@login_required
+def my_protected_view(request):
+    return render(request, 'protected.html', {'current_user': request.user})
+'''
 @api_view(['GET'])
 def getData(request):
     token = request.META.get('HTTP_TOKEN')
     if token == 'bob':
-        items = User.objects.all()
-        serializer = UserSerializer(items, many=True)
+        users = User.objects.all()
+        serializer = UserSerializer(users, many=True)
         return Response(serializer.data)
     else:
         return request.META.get("Invalid token")
 
 @api_view(['POST'])
 def addUser(request):
-    serializer = UserSerializer(data=request.data)
-    #password = str(PasswordHasher.encode("password", salt))
+    password_hasher = PBKDF2WrappedSHA1PasswordHasher()
+    data = request.data
+    data["password"] = password_hasher.encode(data["password"], salt.salt)
+    serializer = UserSerializer(data=data)
     try:
         if serializer.is_valid():
             serializer.save()
             print(serializer)
+        else:
+            print('invalid')
+            return Response(serializer.errors)
     except Exception as e:
         traceback.print_exc()
         print(serializer)
+        return Response(e)
+
     return Response(serializer.data)
